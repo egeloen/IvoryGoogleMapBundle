00bfc74419f9f0f26b84d67d6ff91d0dd399eb23
Eric GELOEN <geloen.eric@gmail.com>
2011-08-04 14:15:35 +0200

    Add point helper class to the templating

---
+++ Templating/Helper/PointHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Templating\Helper;
+
+use Ivory\GoogleMapBundle\Model\Point;
+
+/**
+ * Point helper allows easy rendering
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class PointHelper
+{
+    /**
+     * Renders the point
+     *
+     * @param Ivory\GoogleMapBundle\Model\Point $point
+     * @return string HTML output
+     */
+    public function render(Point $point)
+    {
+        return sprintf('new google.maps.Point(%s, %s)',
+            $point->getX(),
+            $point->getY()
+        );
+    }
+}
