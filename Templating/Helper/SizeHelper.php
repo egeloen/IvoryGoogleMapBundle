b1a783a6e3a24871629b0e5b0ececa603bd495f4
Eric GELOEN <geloen.eric@gmail.com>
2011-08-04 14:19:52 +0200

    Add size helper class to the templating

---
+++ Templating/Helper/SizeHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Templating\Helper;
+
+use Ivory\GoogleMapBundle\Model\Size;
+
+/**
+ * Size helper allows easy rendering
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class SizeHelper
+{
+    /**
+     * Renders the size
+     *
+     * @param Ivory\GoogleMapBundle\Model\Size $size
+     * @return string HTML output
+     */
+    public function render(Size $size)
+    {
+        if($size->hasUnits())
+            return sprintf('new google.maps.Size(%s, %s, "%s", "%s")',
+                $size->getWidth(),
+                $size->getHeight(),
+                $size->getWidthUnit(),
+                $size->getHeightUnit()
+            );
+        else
+            return sprintf('new google.maps.Size(%s, %s)',
+                $size->getWidth(),
+                $size->getHeight()
+            );
+    }
+}
