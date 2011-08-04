c5925d498e636217ceaa2d9f960ae28b244d23b4
Eric GELOEN <geloen.eric@gmail.com>
2011-08-04 15:48:43 +0200

    Update setter to allow different prototype

--- Model/MarkerImage.php
+++ Model/MarkerImage.php
@@ -88,11 +88,27 @@ class MarkerImage extends AbstractAsset
     /**
      * Sets the anchor of the marker image
      *
-     * @param Ivory\GoogleMapBundle\Model\Point $anchor 
+     * Available prototype:
+     * 
+     * public function setAnchor(integer x, integer y)
+     * public function setAnchor(Ivory\GoogleMapBundle\Model\Point $anchor)
      */
-    public function setAnchor(Point $anchor)
+    public function setAnchor()
     {
-        $this->anchor = $anchor;
+        $args = func_get_args();
+        
+        if(isset($args[0]) && is_int($args[0]) && isset($args[1]) && is_int($args[1]))
+        {
+            if($this->anchor === null)
+                $this->anchor = new Point();
+            
+            $this->anchor->setX($args[0]);
+            $this->anchor->setY($args[1]);
+        }
+        else if($args[0] instanceof Point)
+            $this->anchor = $anchor;
+        else
+            throw new \InvalidArgumentException();
     }
     
     /**
@@ -118,11 +134,27 @@ class MarkerImage extends AbstractAsset
     /**
      * Sets the origin of the marker image
      *
-     * @param Ivory\GoogleMapBundle\Model\Point $origin 
+     * Available prototype:
+     * 
+     * public function setOrigin(integer x, integer y)
+     * public function setOrigin(Ivory\GoogleMapBundle\Model\Point $anchor)
      */
-    public function setOrigin(Point $origin)
+    public function setOrigin()
     {
-        $this->origin = $origin;
+        $args = func_get_args();
+        
+        if(isset($args[0]) && is_int($args[0]) && isset($args[1]) && is_int($args[1]))
+        {
+            if($this->origin === null)
+                $this->origin = new Point();
+            
+            $this->origin->setX($args[0]);
+            $this->origin->setY($args[1]);
+        }
+        else if(isset($args[0]) && ($args[0] instanceof Point))
+            $this->origin = $anchor;
+        else
+            throw new \InvalidArgumentException();
     }
     
     /**
@@ -148,11 +180,31 @@ class MarkerImage extends AbstractAsset
     /**
      * Sets the scaled size of the marker image
      *
-     * @param Ivory\GoogleMapBundle\Model\Size $scaledSize 
+     * Available prototype:
+     * 
+     * public function setScaledSize(integer $width, integer $height, string $widthUnit = null, string $heightUnit = null)
+     * public function setScaledSize(Size $scaledSize)
      */
-    public function setScaledSize(Size $scaledSize)
+    public function setScaledSize()
     {
-        $this->scaledSize = $scaledSize;
+        if(isset($args[0]) && is_int($args[0]) && isset($args[1]) && is_int($args[1]))
+        {
+            if($this->scaledSize === null)
+                $this->scaledSize = new Size($args[0], $args[1]);
+            
+            $this->scaledSize->setWidth($args[0]);
+            $this->scaledSize->setHeight($args[1]);
+            
+            if(isset($args[2]) && is_string($args[2]) && isset($args[3]) && is_string($args[3]))
+            {
+                $this->scaledSize->setWidthUnit($args[2]);
+                $this->scaledSize->setHeightUnit($args[3]);
+            }
+        }
+        else if(isset($args[0]) && ($args[0] instanceof Size))
+            $this->scaledSize = $scaledSize;
+        else
+            throw new \InvalidArgumentException();
     }
     
     /**
@@ -178,10 +230,30 @@ class MarkerImage extends AbstractAsset
     /**
      * Sets the size of the marker image
      *
-     * @param Ivory\GoogleMapBundle\Model\Size $size 
+     * Available prototype:
+     * 
+     * public function setSize(integer $width, integer $height, string $widthUnit = null, string $heightUnit = null)
+     * public function setSize(Size $scaledSize)
      */
-    public function setSize(Size $size)
+    public function setSize()
     {
-        $this->size = $size;
+        if(isset($args[0]) && is_int($args[0]) && isset($args[1]) && is_int($args[1]))
+        {
+            if($this->size === null)
+                $this->size = new Size($args[0], $args[1]);
+            
+            $this->size->setWidth($args[0]);
+            $this->size->setHeight($args[1]);
+            
+            if(isset($args[2]) && is_string($args[2]) && isset($args[3]) && is_string($args[3]))
+            {
+                $this->size->setWidthUnit($args[2]);
+                $this->size->setHeightUnit($args[3]);
+            }
+        }
+        else if(isset($args[0]) && ($args[0] instanceof Size))
+            $this->size = $scaledSize;
+        else
+            throw new \InvalidArgumentException();
     }
 }
