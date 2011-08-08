6b5d1bc8a3f9539a993d09343a0a109462b3be04
Eric GELOEN <geloen.eric@gmail.com>
2011-08-08 16:12:57 +0200

    Add marker shape helper

---
+++ Templating/Helper/MarkerShapeHelper.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Templating\Helper;
+
+use Ivory\GoogleMapBundle\Model\MarkerShape;
+
+/**
+ * Marker shape helper allows easy rendering
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class MarkerShapeHelper 
+{
+    /**
+     * Renders the marker shape
+     *
+     * @param Ivory\GoogleMapBundle\Model\MarkerShape $markerShape
+     * @return string HTML output
+     */
+    public function render(MarkerShape $markerShape)
+    {
+        return sprintf('var %s = %s;',
+            $markerShape->getJavascriptVariable(),
+            json_encode(array(
+                'type' => $markerShape->getType(),
+                'coords' => $markerShape->getCoordinates()
+            ))
+        );
+    }
+}
