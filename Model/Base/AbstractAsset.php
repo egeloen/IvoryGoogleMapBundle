33d15e1033df7c2e612180b6b3c2f7db5d60ef2b
Eric GELOEN <geloen.eric@gmail.com>
2011-09-26 00:23:45 +0200

    Add AbstractAsset class to base objects

---
+++ Model/Base/AbstractAsset.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Model\Base;
+
+use Ivory\GoogleMapBundle\Model\AbstractAsset as BaseAsset;
+
+/**
+ * @override
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+abstract class AbstractAsset extends BaseAsset
+{
+    
+}
