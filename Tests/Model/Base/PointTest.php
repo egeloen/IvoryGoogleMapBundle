50e1950fe004a19db8c0e76c2b22a67c0ef9de2e
Eric GELOEN <geloen.eric@gmail.com>
2011-09-26 16:45:59 +0200

    Add point tests

---
+++ Tests/Model/Base/PointTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Tests\Model\Base;
+
+use Ivory\GoogleMapBundle\Model\Base\Point;
+
+/**
+ * Point test
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class PointTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Ivory\GoogleMapBundle\Model\Base\Point Tested point
+     */
+    protected static $point = null;
+    
+    /**
+     * @override
+     */
+    protected function setUp()
+    {
+        self::$point = new Point();
+    }
+    
+    /**
+     * Checks the point default value
+     */
+    public function testDefaultValues()
+    {
+        $this->assertEquals(self::$point->getX(), 0);
+        $this->assertEquals(self::$point->getY(), 0);
+    }
+    
+    /**
+     * Checks the x getter & setter
+     */
+    public function testX()
+    {
+        self::$point->setX(1.1);
+        $this->assertEquals(self::$point->getX(), 1.1);
+        
+        $this->setExpectedException('InvalidArgumentException');
+        self::$point->setX('foo');
+    }
+    
+    /**
+     * Checks the y getter & setter
+     */
+    public function testLongitude()
+    {
+        self::$point->setY(1.1);
+        $this->assertEquals(self::$point->getY(), 1.1);
+        
+        $this->setExpectedException('InvalidArgumentException');
+        self::$point->setY('foo');
+    }
+}
