3872addedbde448480863463890fb6f1c045df8b
Eric GELOEN <geloen.eric@gmail.com>
2011-09-26 18:14:12 +0200

    Add getter & setter for ground overlay options

--- Model/Overlays/GroundOverlay.php
+++ Model/Overlays/GroundOverlay.php
@@ -115,4 +115,53 @@ class GroundOverlay extends AbstractAsset implements IExtendable
                 ' - public function setBount(Ivory\GoogleMapBundle\Model\Base\Coordinate $southWest, Ivory\GoogleMapBundle\Model\Base\Coordinate $northEast)',
                 ' - public function setBound(double $southWestLatitude, double $southWestLongitude, double $northEastLatitude, double $northEastLongitude, boolean southWestNoWrap = true, boolean $northEastNoWrap = true)'));
     }
+    
+    /**
+     * Gets the ground overlay options
+     *
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * Sets the ground overlay options
+     *
+     * @param array $options
+     */
+    public function setOptions(array $options)
+    {
+        foreach($options as $option => $value)
+            $this->setOption($option, $value);
+    }
+
+    /**
+     * Gets a specific ground overlay option
+     *
+     * @param string $option
+     * @return mixed
+     */
+    public function getOption($option)
+    {
+        if(is_string($option))
+            return isset($this->options[$option]) ? $this->options[$option] : null;
+        else
+            throw new \InvalidArgumentException('The option property of a ground overlay must be a string value.');
+    }
+
+    /**
+     * Sets a specific ground overlay option
+     *
+     * @param string $option
+     * @param mixed $value
+     */
+    public function setOption($option, $value)
+    {
+        if(is_string($option))
+            $this->options[$option] = $value;
+        else
+            throw new \InvalidArgumentException('The option property of a ground overlay must be a string value.');
+    }
 }
