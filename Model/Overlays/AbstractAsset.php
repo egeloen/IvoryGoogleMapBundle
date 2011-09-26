081618c7bcde556c18b4b581a1aa4656ce885fab
Eric GELOEN <geloen.eric@gmail.com>
2011-09-26 13:08:04 +0200

    Add AbstractAsset overlay class

---
+++ Model/Overlays/AbstractAsset.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Model\Overlays;
+
+use Ivory\GoogleMapBundle\Model\AbstractAsset as BaseAsset;
+
+/**
+ * @override
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+abstract class AbstractAsset extends BaseAsset
+{
+    
+}
