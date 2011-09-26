1d5b827de46ddccce69364ef656d806cd9ee9abd
Eric GELOEN <geloen.eric@gmail.com>
2011-09-26 11:34:28 +0200

    Add Iextendable class which describes a class that can be extended by a bound

---
+++ Model/Overlays/IExtendable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Model\Overlays;
+
+/**
+ * Each class implements this interface can be extend by a bound
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+interface IExtendable 
+{
+    
+}
