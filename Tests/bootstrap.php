8b3df3f83e06b70d81a22518ffd79794fbb7e057
Eric GELOEN <geloen.eric@gmail.com>
2011-09-26 16:41:31 +0200

    Add bootstrap test

---
+++ Tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+
+$loader = new UniversalClassLoader();
+$loader->registerNamespaces(array(
+    'Symfony' => $_SERVER['SYMFONY'],
+    'Ivory'   => $_SERVER['BUNDLE']
+));
+$loader->register();
