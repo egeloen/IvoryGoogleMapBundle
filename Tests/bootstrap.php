44edb116b432889addb0c46799abb6dad545705c
Eric GELOEN <geloen.eric@gmail.com>
2011-09-26 21:20:32 +0200

    Remobe bundle server variable from phpunit config & update bootstrap to register the bundle automatically

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -4,9 +4,11 @@ require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassL
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
+var_dump(__DIR__);
+
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
     'Symfony' => $_SERVER['SYMFONY'],
-    'Ivory'   => $_SERVER['BUNDLE']
+    'Ivory'   => __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'
 ));
 $loader->register();
