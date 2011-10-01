57cac0a29355ae4c77982c80825613074f812604
Eric GELOEN <geloen.eric@gmail.com>
2011-10-01 18:48:12 +0200

    Add map type control style class & test it

---
+++ Tests/Model/Controls/MapTypeControlStyleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Tests\Model\Controls;
+
+use Ivory\GoogleMapBundle\Model\Controls\MapTypeControlStyle;
+
+/**
+ * Map type control style test
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class MapTypeControlStyleTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Checks the map type control styles getter
+     */
+    public function testMapTypeControlStyles()
+    {
+        $this->assertEquals(MapTypeControlStyle::getMapTypeControlStyles(), array(
+            MapTypeControlStyle::DEFAULT_,
+            MapTypeControlStyle::DROPDOWN_MENU,
+            MapTypeControlStyle::HORIZONTAL_BAR
+        ));
+    }
+}
