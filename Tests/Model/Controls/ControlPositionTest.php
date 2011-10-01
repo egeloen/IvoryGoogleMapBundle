5e1b5cb79b8939c5e4bde0f9070ffba7c83f4482
Eric GELOEN <geloen.eric@gmail.com>
2011-10-01 18:39:06 +0200

    Add control position class & test it

---
+++ Tests/Model/Controls/ControlPositionTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Tests\Model\Controls;
+
+use Ivory\GoogleMapBundle\Model\Controls\ControlPosition;
+
+/**
+ * Control position test
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class ControlPositionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Checks the map type ids getter
+     */
+    public function testControlPositions()
+    {
+        $this->assertEquals(ControlPosition::getControlPositions(), array(
+            ControlPosition::BOTTOM_CENTER,
+            ControlPosition::BOTTOM_LEFT,
+            ControlPosition::BOTTOM_RIGHT,
+            ControlPosition::LEFT_BOTTOM,
+            ControlPosition::LEFT_CENTER,
+            ControlPosition::LEFT_TOP,
+            ControlPosition::RIGHT_BOTTOM,
+            ControlPosition::RIGHT_CENTER,
+            ControlPosition::RIGHT_TOP,
+            ControlPosition::TOP_CENTER,
+            ControlPosition::TOP_LEFT,
+            ControlPosition::TOP_RIGHT
+        ));
+    }
+}
