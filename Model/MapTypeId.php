305e32db2ea2f4749868cd1b725494e46f111837
Eric GELOEN <geloen.eric@gmail.com>
2011-10-01 17:40:23 +0200

    Add map type Id class which defines the available map types constant

---
+++ Model/MapTypeId.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Model;
+
+/**
+ * Map type ID which describes a google map type id
+ *
+ * @see http://code.google.com/apis/maps/documentation/javascript/reference.html#MapTypeId
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class MapTypeId 
+{
+    const HYBRID = 'hybrid';
+    const ROADMAP = 'roadmap';
+    const SATELLITE = 'satellite';
+    const TERRAIN = 'terrain';
+    
+    /**
+     * Disabled constructor
+     */
+    final public function __construct()
+    {
+        throw new \Exception(sprintf('The class "%s" can not be instanciate.', get_class($this)));
+    }
+}
