5775fe6f248f5f5d0942382ae6456f89a034037b
Eric GELOEN <geloen.eric@gmail.com>
2011-10-04 01:52:59 +0200

    Add rotate control class & test it

---
+++ Model/Controls/RotateControl.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Model\Controls;
+
+/**
+ * A rotate control describes a google map rotate control
+ *
+ * @see http://code.google.com/apis/maps/documentation/javascript/reference.html#RotateControlOptions
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class RotateControl
+{
+    /**
+     * @var string Control position
+     */
+    protected $controlPosition = ControlPosition::TOP_LEFT;
+    
+    /**
+     * Gets the control position
+     *
+     * @return string
+     */
+    public function getControlPosition()
+    {
+        return $this->controlPosition;
+    }
+    
+    /**
+     * Sets the control position
+     *
+     * @param string $controlPosition 
+     */
+    public function setControlPosition($controlPosition)
+    {
+        if(in_array($controlPosition, ControlPosition::getControlPositions()))
+            $this->controlPosition = $controlPosition;
+        else
+            throw new \InvalidArgumentException(sprintf('The control position of a rotate control can only be : %s.', implode(', ', ControlPosition::getControlPositions())));
+    }
+}
---
+++ Tests/Model/Controls/RotateControlTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Tests\Model\Controls;
+
+use Ivory\GoogleMapBundle\Model\Controls\RotateControl;
+use Ivory\GoogleMapBundle\Model\Controls\ControlPosition;
+
+/**
+ * Rotate control test
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class RotateControlTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Ivory\GoogleMapBundle\Model\Controls\RotateControl Tested rotate control
+     */
+    protected static $rotateControl = null;
+    
+    /**
+     * @override
+     */
+    protected function setUp()
+    {
+        self::$rotateControl = new RotateControl();
+    }
+    
+    /**
+     * Checks the rotate control default value
+     */
+    public function testDefaultValues()
+    {
+        $this->assertEquals(self::$rotateControl->getControlPosition(), 'top_left');
+    }
+    
+    /**
+     * Checks the control position getter & setter
+     */
+    public function testControlPosition()
+    {
+        self::$rotateControl->setControlPosition(ControlPosition::BOTTOM_CENTER);
+        $this->assertEquals(self::$rotateControl->getControlPosition(), 'bottom_center');
+        
+        $this->setExpectedException('InvalidArgumentException');
+        self::$rotateControl->setControlPosition('foo');
+    }
+}
