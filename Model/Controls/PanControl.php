51d5b5b96968d579edcb24fc1045306990f1e62c
Eric GELOEN <geloen.eric@gmail.com>
2011-10-04 01:06:11 +0200

    Add pan control class & test it

---
+++ Model/Controls/PanControl.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Model\Controls;
+
+/**
+ * A pan control describes a google map pan control
+ *
+ * @see http://code.google.com/apis/maps/documentation/javascript/reference.html#PanControlOptions
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class PanControl
+{
+    /**
+     * @var string Control position
+     */
+    protected $controlPosition = ControlPosition::TOP_LEFT;
+    
+    /**
+     * Gets the control position
+     *
+     * @return string
+     */
+    public function getControlPosition()
+    {
+        return $this->controlPosition;
+    }
+    
+    /**
+     * Sets the control position
+     *
+     * @param string $controlPosition 
+     */
+    public function setControlPosition($controlPosition)
+    {
+        if(in_array($controlPosition, ControlPosition::getControlPositions()))
+            $this->controlPosition = $controlPosition;
+        else
+            throw new \InvalidArgumentException(sprintf('The control position of a pan control can only be : %s.', implode(', ', ControlPosition::getControlPositions())));
+    }
+}
