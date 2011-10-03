238d9345c37605fb242b80a6d7dc6987abb6ed89
Eric GELOEN <geloen.eric@gmail.com>
2011-10-03 23:51:54 +0200

    Add overview map control class & test it

---
+++ Model/Controls/OverviewMapControl.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Model\Controls;
+
+/**
+ * An overview map control describes a google map overview control
+ *
+ * @see http://code.google.com/apis/maps/documentation/javascript/reference.html#OverviewMapControlOptions
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class OverviewMapControl 
+{
+    /**
+     * @var boolean TRUE if the overview map control is opened else FALSE
+     */
+    protected $opened = false;
+    
+    /**
+     * Checks if the overview map control is opened else FALSE
+     *
+     * @return boolean TRUE if the overview map control is opened else FALSE
+     */
+    public function isOpened()
+    {
+        return $this->opened;
+    }
+    
+    /**
+     * Sets if the overview map control is opened
+     *
+     * @param boolean $opened TRUE if the overview map control is opened else FALSE
+     */
+    public function setOpened($opened)
+    {
+        if(is_bool($opened))
+            $this->opened = $opened;
+        else
+            throw new \InvalidArgumentException('The opened property of an overview map control must be a boolean value.');
+    }
+}
---
+++ Tests/Model/Controls/OverviewMapControlTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Tests\Model\Controls;
+
+use Ivory\GoogleMapBundle\Model\Controls\OverviewMapControl;
+
+/**
+ * Overview map control test
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class OverviewMapControlTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Ivory\GoogleMapBundle\Model\Controls\OverviewMapControl Tested overview map control
+     */
+    protected static $overviewMapControl = null;
+    
+    /**
+     * @override
+     */
+    protected function setUp()
+    {
+        self::$overviewMapControl = new OverviewMapControl();
+    }
+    
+    /**
+     * Checks the map type control default value
+     */
+    public function testDefaultValues()
+    {
+        $this->assertFalse(self::$overviewMapControl->isOpened());
+    }
+    
+    /**
+     * Checks the opened getter & setter
+     */
+    public function testOpened()
+    {
+        self::$overviewMapControl->setOpened(true);
+        $this->assertTrue(self::$overviewMapControl->isOpened());
+        
+        $this->setExpectedException('InvalidArgumentException');
+        self::$overviewMapControl->setOpened('foo');
+    }
+}
