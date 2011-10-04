7fb2a8ac30aeac55c71214e512e296e788efb339
Eric GELOEN <geloen.eric@gmail.com>
2011-10-04 13:53:14 +0200

    Add scale control class & test it

---
+++ Model/Controls/ScaleControl.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Model\Controls;
+
+use Ivory\GoogleMapBundle\Model\Controls\ControlPosition;
+use Ivory\GoogleMapBundle\Model\Controls\ScaleControlStyle;
+
+/**
+ * Scale control options describes a google map scale control options
+ *
+ * @see http://code.google.com/apis/maps/documentation/javascript/reference.html#ScaleControlOptions
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class ScaleControl
+{
+    /**
+     * @var string Control position
+     */
+    protected $controlPosition = ControlPosition::BOTTOM_LEFT;
+    
+    /**
+     * @var string Scale control style
+     */
+    protected $scaleControlStyle = ScaleControlStyle::DEFAULT_;
+    
+    /**
+     * Gets the control position
+     *
+     * @return string
+     */
+    public function getControlPosition()
+    {
+        return $this->controlPosition;
+    }
+    
+    /**
+     * Sets the control position
+     *
+     * @param string $controlPosition 
+     */
+    public function setControlPosition($controlPosition)
+    {
+        if(in_array($controlPosition, ControlPosition::getControlPositions()))
+            $this->controlPosition = $controlPosition;
+        else
+            throw new \InvalidArgumentException(sprintf('The control position of a map type control can only be : %s.', implode(', ', ControlPosition::getControlPositions())));
+    }
+    
+    /**
+     * Gets the scale control style
+     *
+     * @return string
+     */
+    public function getScaleControlStyle()
+    {
+        return $this->scaleControlStyle;
+    }
+    
+    /**
+     * Sets the scale control style
+     *
+     * @param type $scaleControlStyle 
+     */
+    public function setScaleControlStyle($scaleControlStyle)
+    {
+        if(in_array($scaleControlStyle, ScaleControlStyle::getScaleControlStyles()))
+            $this->scaleControlStyle = $scaleControlStyle;
+        else
+            throw new \InvalidArgumentException(sprintf('The scale control style of a scale control can only be : %s.', implode(', ', ScaleControlStyle::getScaleControlStyles())));
+    }
+}
---
+++ Tests/Model/Controls/ScaleControlTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Tests\Model\Controls;
+
+use Ivory\GoogleMapBundle\Model\Controls\ScaleControl;
+use Ivory\GoogleMapBundle\Model\Controls\ControlPosition;
+use Ivory\GoogleMapBundle\Model\Controls\ScaleControlStyle;
+
+/**
+ * Scale control test
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class ScaleControlTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Ivory\GoogleMapBundle\Model\Controls\ScaleControl Tested scale control
+     */
+    protected static $scaleControl = null;
+    
+    /**
+     * @override
+     */
+    protected function setUp()
+    {
+        self::$scaleControl = new ScaleControl();
+    }
+    
+    /**
+     * Checks the map type control default value
+     */
+    public function testDefaultValues()
+    {
+        $this->assertEquals(self::$scaleControl->getControlPosition(), 'bottom_left');
+        $this->assertEquals(self::$scaleControl->getScaleControlStyle(), 'default');
+    }
+    
+    /**
+     * Checks the control position getter & setter
+     */
+    public function testControlPosition()
+    {
+        self::$scaleControl->setControlPosition(ControlPosition::BOTTOM_CENTER);
+        $this->assertEquals(self::$scaleControl->getControlPosition(), 'bottom_center');
+        
+        $this->setExpectedException('InvalidArgumentException');
+        self::$scaleControl->setControlPosition('foo');
+    }
+    
+    /**
+     * Checks the scale control style getter & setter
+     */
+    public function testScaleControlStyle()
+    {
+        self::$scaleControl->setScaleControlStyle(ScaleControlStyle::DEFAULT_);
+        $this->assertEquals(self::$scaleControl->getScaleControlStyle(), 'default');
+        
+        $this->setExpectedException('InvalidArgumentException');
+        self::$scaleControl->setScaleControlStyle('foo');
+    }
+}
