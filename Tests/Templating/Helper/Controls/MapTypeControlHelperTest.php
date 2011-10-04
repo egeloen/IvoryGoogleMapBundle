7749bad25468240c29b3eb062ee77cf025d1e60b
Eric GELOEN <geloen.eric@gmail.com>
2011-10-04 13:39:15 +0200

    Fix mistake in test files naming

--- Tests/Templating/Helper/Controls/MapTypeControlHelperTest.php
+++ Tests/Templating/Helper/Controls/MapTypeControlHelperTest.php
@@ -2,27 +2,34 @@
 
 namespace Ivory\GoogleMapBundle\Tests\Templating\Helper\Controls;
 
+use Ivory\GoogleMapBundle\Templating\Helper\Controls\MapTypeControlHelper;
+use Ivory\GoogleMapBundle\Templating\Helper\MapTypeIdHelper;
+use Ivory\GoogleMapBundle\Templating\Helper\Controls\ControlPositionHelper;
 use Ivory\GoogleMapBundle\Templating\Helper\Controls\MapTypeControlStyleHelper;
+
+use Ivory\GoogleMapBundle\Model\Controls\MapTypeControl;
+use Ivory\GoogleMapBundle\Model\MapTypeId;
+use Ivory\GoogleMapBundle\Model\Controls\ControlPosition;
 use Ivory\GoogleMapBundle\Model\Controls\MapTypeControlStyle;
 
 /**
- * Map type control style helper test
+ * Map type control helper test
  *
  * @author GeLo <geloen.eric@gmail.com>
  */
-class MapTypeControlStyleHelperTest extends \PHPUnit_Framework_TestCase
+class MapTypeControlHelperTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var Ivory\GoogleMapBundle\Templating\Helper\Controls\MapTypeControlStyleHelper
+     * @var Ivory\GoogleMapBundle\Templating\Helper\Controls\MapTypeControlHelper
      */
-    protected static $mapTypeControlStyleHelper = null;
+    protected static $mapTypeControlHelper = null;
     
     /**
      * @override
      */
     protected function setUp()
     {
-        self::$mapTypeControlStyleHelper = new MapTypeControlStyleHelper();
+        self::$mapTypeControlHelper = new MapTypeControlHelper(new MapTypeIdHelper(), new ControlPositionHelper(), new MapTypeControlStyleHelper());
     }
     
     /**
@@ -30,11 +37,11 @@ class MapTypeControlStyleHelperTest extends \PHPUnit_Framework_TestCase
      */
     public function testRender()
     {
-        $this->assertEquals(self::$mapTypeControlStyleHelper->render(MapTypeControlStyle::DEFAULT_), 'google.maps.MapTypeControlStyle.DEFAULT');
-        $this->assertEquals(self::$mapTypeControlStyleHelper->render(MapTypeControlStyle::DROPDOWN_MENU), 'google.maps.MapTypeControlStyle.DROPDOWN_MENU');
-        $this->assertEquals(self::$mapTypeControlStyleHelper->render(MapTypeControlStyle::HORIZONTAL_BAR), 'google.maps.MapTypeControlStyle.HORIZONTAL_BAR');
+        $mapTypeControlTest = new MapTypeControl();
+        $mapTypeControlTest->setMapTypeIds(array(MapTypeId::ROADMAP));
+        $mapTypeControlTest->setControlPosition(ControlPosition::BOTTOM_CENTER);
+        $mapTypeControlTest->setMapTypeControlStyle(MapTypeControlStyle::DROPDOWN_MENU);
         
-        $this->setExpectedException('InvalidArgumentException');
-        self::$mapTypeControlStyleHelper->render('foo');
+        $this->assertEquals(self::$mapTypeControlHelper->render($mapTypeControlTest), '{"mapTypeIds":[google.maps.MapTypeId.ROADMAP],"position":google.maps.ControlPosition.BOTTOM_CENTER,"style":google.maps.MapTypeControlStyle.DROPDOWN_MENU}');
     }
 }
--- Tests/Templating/Helper/Controls/MapTypeControlStyleHelperTest.php
+++ Tests/Templating/Helper/Controls/MapTypeControlStyleHelperTest.php
@@ -2,34 +2,27 @@
 
 namespace Ivory\GoogleMapBundle\Tests\Templating\Helper\Controls;
 
-use Ivory\GoogleMapBundle\Templating\Helper\Controls\MapTypeControlHelper;
-use Ivory\GoogleMapBundle\Templating\Helper\MapTypeIdHelper;
-use Ivory\GoogleMapBundle\Templating\Helper\Controls\ControlPositionHelper;
 use Ivory\GoogleMapBundle\Templating\Helper\Controls\MapTypeControlStyleHelper;
-
-use Ivory\GoogleMapBundle\Model\Controls\MapTypeControl;
-use Ivory\GoogleMapBundle\Model\MapTypeId;
-use Ivory\GoogleMapBundle\Model\Controls\ControlPosition;
 use Ivory\GoogleMapBundle\Model\Controls\MapTypeControlStyle;
 
 /**
- * Map type control helper test
+ * Map type control style helper test
  *
  * @author GeLo <geloen.eric@gmail.com>
  */
-class MapTypeControlHelperTest extends \PHPUnit_Framework_TestCase
+class MapTypeControlStyleHelperTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var Ivory\GoogleMapBundle\Templating\Helper\Controls\MapTypeControlHelper
+     * @var Ivory\GoogleMapBundle\Templating\Helper\Controls\MapTypeControlStyleHelper
      */
-    protected static $mapTypeControlHelper = null;
+    protected static $mapTypeControlStyleHelper = null;
     
     /**
      * @override
      */
     protected function setUp()
     {
-        self::$mapTypeControlHelper = new MapTypeControlHelper(new MapTypeIdHelper(), new ControlPositionHelper(), new MapTypeControlStyleHelper());
+        self::$mapTypeControlStyleHelper = new MapTypeControlStyleHelper();
     }
     
     /**
@@ -37,11 +30,11 @@ class MapTypeControlHelperTest extends \PHPUnit_Framework_TestCase
      */
     public function testRender()
     {
-        $mapTypeControlTest = new MapTypeControl();
-        $mapTypeControlTest->setMapTypeIds(array(MapTypeId::ROADMAP));
-        $mapTypeControlTest->setControlPosition(ControlPosition::BOTTOM_CENTER);
-        $mapTypeControlTest->setMapTypeControlStyle(MapTypeControlStyle::DROPDOWN_MENU);
+        $this->assertEquals(self::$mapTypeControlStyleHelper->render(MapTypeControlStyle::DEFAULT_), 'google.maps.MapTypeControlStyle.DEFAULT');
+        $this->assertEquals(self::$mapTypeControlStyleHelper->render(MapTypeControlStyle::DROPDOWN_MENU), 'google.maps.MapTypeControlStyle.DROPDOWN_MENU');
+        $this->assertEquals(self::$mapTypeControlStyleHelper->render(MapTypeControlStyle::HORIZONTAL_BAR), 'google.maps.MapTypeControlStyle.HORIZONTAL_BAR');
         
-        $this->assertEquals(self::$mapTypeControlHelper->render($mapTypeControlTest), '{"mapTypeIds":[google.maps.MapTypeId.ROADMAP],"position":google.maps.ControlPosition.BOTTOM_CENTER,"style":google.maps.MapTypeControlStyle.DROPDOWN_MENU}');
+        $this->setExpectedException('InvalidArgumentException');
+        self::$mapTypeControlStyleHelper->render('foo');
     }
 }
