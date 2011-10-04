0a5bb2894c0caf26d2b7fd4501f1b20f36b1c2aa
Eric GELOEN <geloen.eric@gmail.com>
2011-10-04 14:40:58 +0200

    Add street view control class & test it

---
+++ Tests/Model/Controls/StreetViewControlTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Tests\Model\Controls;
+
+use Ivory\GoogleMapBundle\Model\Controls\StreetViewControl;
+use Ivory\GoogleMapBundle\Model\Controls\ControlPosition;
+
+/**
+ * Street view control test
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class StreetViewControlTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Ivory\GoogleMapBundle\Model\Controls\StreetViewControl Tested street view control
+     */
+    protected static $streetViewControl = null;
+    
+    /**
+     * @override
+     */
+    protected function setUp()
+    {
+        self::$streetViewControl = new StreetViewControl();
+    }
+    
+    /**
+     * Checks the street view control default value
+     */
+    public function testDefaultValues()
+    {
+        $this->assertEquals(self::$streetViewControl->getControlPosition(), 'top_left');
+    }
+    
+    /**
+     * Checks the control position getter & setter
+     */
+    public function testControlPosition()
+    {
+        self::$streetViewControl->setControlPosition(ControlPosition::BOTTOM_CENTER);
+        $this->assertEquals(self::$streetViewControl->getControlPosition(), 'bottom_center');
+        
+        $this->setExpectedException('InvalidArgumentException');
+        self::$streetViewControl->setControlPosition('foo');
+    }
+}
