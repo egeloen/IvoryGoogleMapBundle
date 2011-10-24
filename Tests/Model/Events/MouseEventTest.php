b606927222d6c8ca5d6e9d57dffe47499e22bb53
Eric GELOEN <geloen.eric@gmail.com>
2011-10-25 00:00:40 +0200

    Add MouseEvent class which describes the available mouse event + tests

---
+++ Tests/Model/Events/MouseEventTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Tests\Model\Events;
+
+use Ivory\GoogleMapBundle\Model\Events\MouseEvent;
+
+/**
+ * Mouse event test
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class MouseEventTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Checks the disable constructor
+     */
+    public function testConstruct()
+    {
+        $this->setExpectedException('Exception');
+        $mouseEvent = new MouseEvent();
+    }
+    
+    /**
+     * Checks the map type control styles getter
+     */
+    public function testMapTypeControlStyles()
+    {
+        $this->assertEquals(MouseEvent::getMouseEvents(), array(
+            MouseEvent::CLICK,
+            MouseEvent::DBLCLICK,
+            MouseEvent::MOUSEUP,
+            MouseEvent::MOUSEDOWN,
+            MouseEvent::MOUSEOVER,
+            MouseEvent::MOUSEOUT
+        ));
+    }
+}
