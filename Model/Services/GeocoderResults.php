69e934497f8e68ac59bf179875f3659957fcb890
Eric GELOEN <geloen.eric@gmail.com>
2011-11-01 16:43:58 +0100

    Create geocoder service model

---
+++ Model/Services/GeocoderResults.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Model\Services;
+
+/**
+ * Geocoder results wraps the geocoder results & the request status
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class GeocoderResults
+{
+    /**
+     * @var array Geocoder results
+     */
+    protected $results = array();
+    
+    /**
+     * @var string Geocoder results status
+     */
+    protected $status = null;
+    
+    /**
+     * Create a geocoder results
+     *
+     * @param array $results
+     * @param string $status 
+     */
+    public function __construct(array $results, $status)
+    {
+        $this->setResults($results);
+        $this->setStatus($status);
+    }
+    
+    /**
+     * Gets the geocoder results
+     *
+     * @return array
+     */
+    public function getResults()
+    {
+        return $this->results;
+    }
+    
+    /**
+     * Sets the geocoder results
+     *
+     * @param array $results 
+     */
+    public function setResults(array $results)
+    {
+        $this->results = array();
+        
+        foreach($results as $result)
+            $this->addResult($result);
+    }
+    
+    /**
+     * Add a geocoder result
+     *
+     * @param Ivory\GoogleMapBundle\Model\Services\GeocoderResult $result 
+     */
+    public function addResult(GeocoderResult $result)
+    {
+        $this->results[] = $result;
+    }
+    
+    /**
+     * Gets the geocoder results status
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+    
+    /**
+     * Sets the geocoder results status
+     *
+     * @param string $status 
+     */
+    public function setStatus($status)
+    {
+        if(in_array($status, GeocoderStatus::getGeocoderStatus()))
+            $this->status = $status;
+        else
+            throw new \InvalidArgumentException('The geocoder results status can only be : '.implode(', ', GeocoderStatus::getGeocoderStatus ()));
+    }
+}
---
+++ Model/Services/GeocoderStatus.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Model\Services;
+
+/**
+ * Geocoder status which describes a google map geocoder status
+ *
+ * @see http://code.google.com/apis/maps/documentation/javascript/reference.html#GeocoderStatus
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class GeocoderStatus 
+{
+    const ERROR = 'ERROR';
+    const INVALID_REQUEST = 'INVALID_REQUEST';
+    const OK = 'OK';
+    const OVER_QUERY_LIMIT = 'OVER_QUERY_LIMIT';
+    const REQUEST_DENIED = 'REQUEST_DENIED';
+    const UNKNOWN_ERROR = 'UNKNOWN_ERROR';
+    const ZERO_RESULTS = 'ZERO_RESULTS';
+    
+    /**
+     * Gets the available geocoder status
+     *
+     * @return array
+     */
+    public static function getGeocoderStatus()
+    {
+        return array(
+            self::ERROR,
+            self::INVALID_REQUEST,
+            self::OK,
+            self::OVER_QUERY_LIMIT,
+            self::REQUEST_DENIED,
+            self::UNKNOWN_ERROR,
+            self::ZERO_RESULTS
+        );
+    }
+}
