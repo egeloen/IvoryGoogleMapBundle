059e955528d35648f0dfdd6bd0bb089d25a41c04
Eric GELOEN <geloen.eric@gmail.com>
2011-11-23 14:37:54 +0100

    Add encoding helper

---
+++ Templating/Helper/Geometry/EncodingHelper.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Templating\Helper\Geometry;
+
+/**
+ * Encoding Helper
+ *
+ * @see http://code.google.com/apis/maps/documentation/javascript/reference.html#encoding
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class EncodingHelper
+{
+    /**
+     * Renders the decode path method
+     * 
+     * @param string $encodedPath
+     */
+    public function renderDeocodePath($encodedPath)
+    {
+        if(is_string($encodedPath))
+            return sprintf('google.maps.geometry.encoding.decodePath("%s")', $encodedPath);
+        else
+            throw new \InvalidArgumentException('The encoded path must be a string value.');
+    }
+}
---
+++ Tests/Templating/Helper/Geometry/EncodingHelperTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ivory\GoogleMapBundle\Tests\Templating\Helper\Geometry;
+
+use Ivory\GoogleMapBundle\Templating\Helper\Geometry\EncodingHelper;
+
+/**
+ * EncodingHelper test
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class EncodingHelperTest extends \PHPUnit_Framework_TestCase 
+{
+    /**
+     * @var Ivory\GoogleMapBundle\Templating\Helper\Geometry\EncodingHelper
+     */
+    protected static $encodingHelper = null;
+    
+    /**
+     * @override
+     */
+    public function setUp()
+    {
+        self::$encodingHelper = new EncodingHelper();
+    }
+    
+    /**
+     * Checks the render decode path method
+     */
+    public function testRenderDecodePath()
+    {
+        $this->assertEquals(self::$encodingHelper->renderDeocodePath('value'), 'google.maps.geometry.encoding.decodePath("value")');
+    }
+}
